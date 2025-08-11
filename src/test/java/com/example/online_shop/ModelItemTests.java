1abf17a5d6e1881076f320e6cfdf1ff7aefa0929
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-08-11 22:54:57 +0300

    feat: add ModelItemTests

--- src/test/java/com/example/online_shop/ModelItemTests.java
+++ src/test/java/com/example/online_shop/ModelItemTests.java
@@ -12,15 +12,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
-import java.io.File;
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.nio.file.Files;
 import java.util.HashMap;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -49,46 +47,53 @@ class ModelItemTests {
         // Создаём DTO с фиктивной картинкой
         MultipartFile picture = new MockMultipartFile("shop.png", FAKE_IMAGE);
 
-        ItemCreateDto itemCreateDto = ItemCreateDto.builder().title("Товар 1").description("Товар для mock проверки").price(BigDecimal.valueOf(10.50)).image(picture).build();
+        ItemCreateDto itemCreateDto = ItemCreateDto.builder().title("Товар 1").description("Товар для mock проверки")
+                .price(BigDecimal.valueOf(10.50)).image((FilePart) picture).build();
 
-        Item item = Item.builder().title("Товар 1").description("Товар для mock проверки").price(BigDecimal.valueOf(10.50)).image(FAKE_IMAGE).build();
+        Item item = Item.builder().title("Товар 1").description("Товар для mock проверки")
+                .price(BigDecimal.valueOf(10.50)).image(FAKE_IMAGE).build();
 
-        ItemDto itemDto = ItemDto.builder().title("Товар 1").description("Товар для mock проверки").price(BigDecimal.valueOf(10.50)).imagePath(IMAGE_PATH + "1L").build();
+        ItemDto itemDto = ItemDto.builder().title("Товар 1").description("Товар для mock проверки")
+                .price(BigDecimal.valueOf(10.50)).imagePath(IMAGE_PATH + "1L").build();
 
         when(itemMapper.toItem(any(ItemCreateDto.class))).thenReturn(item);
-        when(itemRepository.save(any(Item.class))).thenReturn(item);
+        when(itemRepository.save(any(Item.class))).thenReturn(Mono.just(item));
         when(itemMapper.toDto(any(Item.class))).thenReturn(itemDto);
 
-        ItemDto result = itemService.saveItem(itemCreateDto);
-        assertThat(result).isEqualTo(itemDto);
+        itemService.saveItem(Mono.just(itemCreateDto))
+                .doOnNext(itemRes -> assertThat(itemRes).isEqualTo(itemDto))
+                .subscribe();
     }
 
     @Test
     void testGetImage() {
 
-        Item item = Item.builder().id(1L).title("Товар 1").description("Товар для mock проверки").price(BigDecimal.valueOf(10)).image(FAKE_IMAGE).build();
+        Item item = Item.builder().id(1L).title("Товар 1").description("Товар для mock проверки")
+                .price(BigDecimal.valueOf(10)).image(FAKE_IMAGE).build();
 
-        when(itemRepository.findById(any(Long.class))).thenReturn(Optional.of(item));
+        when(itemRepository.findById(any(Long.class))).thenReturn(Mono.just(item));
 
-        byte[] image = itemService.getImage(1L);
-
-        assertThat(image).isEqualTo(FAKE_IMAGE);
+        itemService.getImage(1L)
+                .doOnNext(image -> assertThat(image).isEqualTo(item.getImage()))
+                .subscribe();
         verify(itemRepository).findById(1L);
     }
 
     @Test
     void testGetItemDtoById() {
 
-        ItemDto itemDto = ItemDto.builder().title("Товар 1").description("Товар для mock проверки").price(BigDecimal.valueOf(10)).imagePath(IMAGE_PATH + "1L").build();
+        ItemDto itemDto = ItemDto.builder().title("Товар 1").description("Товар для mock проверки")
+                .price(BigDecimal.valueOf(10)).imagePath(IMAGE_PATH + "1L").build();
 
-        Item item = Item.builder().id(1L).title("Товар 1").description("Товар для mock проверки").price(BigDecimal.valueOf(10)).image(FAKE_IMAGE).build();
+        Item item = Item.builder().id(1L).title("Товар 1").description("Товар для mock проверки")
+                .price(BigDecimal.valueOf(10)).image(FAKE_IMAGE).build();
 
-        when(itemRepository.findById(any(Long.class))).thenReturn(Optional.of(item));
+        when(itemRepository.findById(any(Long.class))).thenReturn(Mono.just(item));
         when(itemMapper.toDto(any(Item.class))).thenReturn(itemDto);
         when(cartService.getItemsInCart()).thenReturn(new HashMap<>());
 
-        ItemDto itemRes = itemService.getItemDtoById(1L);
-
-        assertThat(itemRes).isEqualTo(itemDto);
+        itemService.getItemDtoById(1L)
+                .doOnNext(itemRes -> assertThat(itemRes).isEqualTo(itemDto))
+                .subscribe();
     }
 }

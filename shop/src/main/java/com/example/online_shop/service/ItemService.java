e2ec77e245d5f14ae7a0a133cdbcea0c35d3144b
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-04 22:34:01 +0300

    feat: add ItemService

--- shop/src/main/java/com/example/online_shop/service/ItemService.java
+++ shop/src/main/java/com/example/online_shop/service/ItemService.java
@@ -2,12 +2,14 @@ package com.example.online_shop.service;
 
 import com.example.online_shop.enumiration.ECartAction;
 import com.example.online_shop.enumiration.ESort;
+import com.example.online_shop.mapper.ItemInCartMapper;
 import com.example.online_shop.mapper.ItemMapper;
 import com.example.online_shop.model.dto.ItemCreateDto;
 import com.example.online_shop.model.dto.ItemDto;
 import com.example.online_shop.model.dto.ItemsWithPagingDto;
 import com.example.online_shop.model.dto.PagingParametersDto;
 import com.example.online_shop.model.entity.Item;
+import com.example.online_shop.model.entity.ItemInCart;
 import com.example.online_shop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,8 @@ import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
@@ -26,94 +30,85 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class ItemService {
     private final ItemRepository itemRepository;
     private final ItemMapper itemMapper;
     private final CartService cartService;
+    private final ItemInCacheService cacheService;
+    private final ItemInCartService itemInCartService;
+    private final ItemInCartMapper itemInCartMapper;
 
     @Value("${shop.items.row:5}")
     int itemsRowCount;
 
-    public Mono<ItemsWithPagingDto> getItems(String search, String sort, int pageNumber, int pageSize) {
-        log.debug("Start getItems: pageNumber={}, pageSize={}", pageNumber, pageSize);
-
-        Pageable page = switch (ESort.valueOf(sort.toUpperCase())) {
-            case NO -> PageRequest.of(pageNumber - 1, pageSize);
-            case ALPHA -> PageRequest.of(pageNumber - 1, pageSize, Sort.by("title"));
-            case PRICE -> PageRequest.of(pageNumber - 1, pageSize, Sort.by("price"));
-        };
-
-        Flux<Item> itemsFlux = (search!= null &&!search.isBlank())? itemRepository.getItemsByTitleLike(search, page)
-                : itemRepository.findBy(page);
-
-        Mono<List<List<ItemDto>>> itemsDto = itemMapper.toListDto(itemsFlux).map(dto -> {
-            dto.setCount(cartService.getItemCountInCart(dto.getId()));
-            return dto;
-        }).collectList().map(list -> {
-            List<List<ItemDto>> grouped = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
-                int groupIndex = i / itemsRowCount;
-                if (grouped.size() <= groupIndex) {
-                    grouped.add(new ArrayList<>());
-                }
-                grouped.get(groupIndex).add(list.get(i));
-            }
-            return grouped;
-        });
-
-        Mono<Boolean> hasNext = itemRepository.count().map(count -> pageNumber < (int) Math.ceil((double) count / pageSize));
-
-        Mono<PagingParametersDto> pagingDto = Mono.just(PagingParametersDto.builder().pageNumber(pageNumber)
-                .pageSize(pageSize).hasPrevious(pageNumber > 1).build())
-                .zipWith(hasNext, (paging, next) -> {
-            paging.setHasNext(next);
-            return paging;
-        }).log();
-
-        return Mono.zip(pagingDto, itemsDto).map(tuple -> {
-            ItemsWithPagingDto result = new ItemsWithPagingDto();
-            result.setPaging(tuple.getT1());
-            result.setItems(tuple.getT2());
-            return result;
-        });
+    public Mono<List<List<ItemDto>>> getItems(String search, String sort, int pageNumber, int pageSize, String login) {
+        log.info("Start getItems: login={}", login);
+        AtomicInteger index = new AtomicInteger();
+        return cacheService.getItems(search, sort, pageNumber, pageSize)
+                .flatMapIterable(itemsList -> itemsList)
+                .log()
+                .flatMap(itemDto -> itemInCartService.getCountByItemIdAndLogin(itemDto.getId(), login).defaultIfEmpty(0)
+                        .zipWith(Mono.just(itemDto), (countInLoginCart, item) -> {
+                            item.setCount(countInLoginCart);
+                            return item;
+                        }))
+                .log()
+                .collectList()
+                .map(itemsListWithCount -> itemsListWithCount.stream()
+                        .collect(Collectors.groupingBy(it -> index.getAndIncrement() / itemsRowCount))
+                        .values()
+                        .stream().toList();
     }
 
-    public Mono<ItemDto> actionWithItemInCart(Long itemId, String action) {
-        Map<Long, ItemDto> itemsInCart = cartService.getItemsInCart();
-        return (itemsInCart.containsKey(itemId) ? Mono.just(itemsInCart.get(itemId)) :
-                getItemDtoById(itemId))
-                .map(item -> {
-                    switch (ECartAction.valueOf(action.toUpperCase())) {
-                        case PLUS -> item.setCount(item.getCount() + 1);
-                        case MINUS -> {
-                            if (item.getCount() >= 1) item.setCount(item.getCount() - 1);
-                        }
-                        case DELETE -> item.setCount(0);
-                    }
-                    if (item.getCount() == 0) itemsInCart.remove(itemId);
-                    else itemsInCart.put(itemId, item);
-                    cartService.refresh(itemsInCart, item);
-                    return item;
+    public Mono<PagingParametersDto> getPaging(String search, String sort, int pageNumber, int pagesize) {
+        return Mono.just(PagingParametersDto.builder()
+                        .pageNumber(pageNumber)
+                        .pageSize(pagesize)
+                        .hasPrevious(pageNumber > 1)
+                        .build())
+                .zipWith(cacheService.getItems(search, sort, pageNumber, pagesize)
+                        .flatMapIterable(itemList -> itemList).count()
+                        .map(count -> pageNumber < Math.ceilDiv(count, pagesize)), (paging, isNextPage) -> {
+                    paging.setHasNext(isNextPage);
+                    return paging;
                 });
     }
 
-    public Mono<ItemDto> getItemDtoById(Long id) {
-        return itemRepository.findById(id)
-                .map(itemMapper::toDto)
-                .map(itemDto -> {
-                    itemDto.setCount(cartService.getItemCountInCart(id));
+
+    public Mono<ItemDto> actionWithItemInCart(Long itemId, String action, String login) {
+        return itemInCartService.getByItemIdAndLogin(itemId, login)
+                .defaultIfEmpty(new ItemInCart())
+                .log()
+                .zipWith(getItemDtoById(itemId, login), (itemInCart, item) -> {
+                    ItemInCart actualItemDto = itemInCart;
+                    if (itemInCart.getCount() == 0) actualItemDto = itemInCartMapper.toItemInCart(item);
+                    actualItemDto.setLogin(login);
+                    return actualItemDto;
+                })
+                .log()
+                .flatMap(itemInCart -> cartService.refresh(itemInCart, action, login))
+                .log();
+    }
+
+    public Mono<ItemDto> getItemDtoById(Long id, String login) {
+        return cacheService.getItemDtoById(id)
+                .zipWith(itemInCartService.getCountByItemIdAndLogin(id, login).defaultIfEmpty(0), (itemDto, count) -> {
+                    itemDto.setCount(count);
                     return itemDto;
                 });
     }
 
+
     public Mono<byte[]> getImage(Long id) {
-        return itemRepository.findById(id)
-                .map(Item::getImage).onErrorComplete();
+        return cacheService.geyImage(id);
     }
 
     @Transactional

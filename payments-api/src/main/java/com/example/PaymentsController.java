19da68ce57e9290f5fd406ff22e1a6dedca3c715
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-05 17:50:34 +0300

    feat:  PaymentsApi

---
+++ payments-api/src/main/java/com/example/PaymentsController.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import main.codegen.com.example.api.DefaultApi;
+
+@RestController
+@Slf4j
+public class PaymentsController implements DefaultApi {
+}

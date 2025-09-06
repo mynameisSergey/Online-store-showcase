34b02b15bb438797751140623f159288458bd0f0
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-07 00:25:02 +0300

    feat: add WebConfiguration

--- shop/src/main/java/com/example/online_shop/configuration/WebConfiguration.java
+++ shop/src/main/java/com/example/online_shop/configuration/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.example.online_shop.configuration;
 
 import com.example.online_shop.model.dto.CartDto;
 import com.example.online_shop.model.dto.ItemDto;
+import com.example.online_shop.model.entity.ItemInCart;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCust
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -16,60 +19,58 @@ import org.springframework.web.context.annotation.SessionScope;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class WebConfiguration {
 
+    private final RedisConnectionFactory redisConnectionFactory;
+    private final Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder;
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
     @Bean
-    public RedisCacheManagerBuilderCustomizer itemCacheCustomizer() {
-        return builder -> builder.withCacheConfiguration(
-                "item",
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofSeconds(5))
-                        .serializeValuesWith(RedisSerializationContext
-                                .SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(ItemDto.class))));
+    public RedisCacheManager cacheManager() {
+        var om = jackson2ObjectMapperBuilder.createXmlMapper(false).build();
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+
+        cacheConfigs.put("item", defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(5))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(ItemDto.class))));
+
+        cacheConfigs.put("picture", defaultCacheConfig()
+                .entryTtl(Duration.ofDays(1))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(byte[].class))));
+
+        cacheConfigs.put("items", defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(5))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(om.getTypeFactory()
+                                .constructCollectionType(List.class, ItemDto.class)))));
+
+        cacheConfigs.put("itemsInCart", defaultCacheConfig()
+                .entryTtl(Duration.ofDays(30))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(om.getTypeFactory()
+                                .constructCollectionType(List.class, ItemInCart.class)))));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultCacheConfig()).withInitialCacheConfigurations(cacheConfigs).build();
     }
 
-    @Bean
-    public RedisCacheManagerBuilderCustomizer itemPictureCacheCustomizer() {
-        return builder -> builder.withCacheConfiguration(
-                "picture",
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofDays(1))
-                        .serializeValuesWith(RedisSerializationContext
-                                .SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(byte[].class))));
+    private RedisCacheConfiguration defaultCacheConfig() {
+        return RedisCacheConfiguration.defaultCacheConfig().disableCachingNullValues();
     }
-
-    @Bean
-    public RedisCacheManagerBuilderCustomizer itemsCacheCustomizer(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
-        var om = jacksonObjectMapperBuilder.createXmlMapper(false).build();
-        return builder -> builder.withCacheConfiguration(
-                "items",
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofSeconds(5))
-                        .serializeValuesWith(RedisSerializationContext
-                                .SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(om.getTypeFactory()
-                                        .constructCollectionType(List.class, ItemDto.class)))));
-    }
-
-    @Bean
-    public RedisCacheManagerBuilderCustomizer itemsInCartCacheCustomizer(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
-        var om = jacksonObjectMapperBuilder.createXmlMapper(false).build();
-        return builder -> builder.withCacheConfiguration(
-                "itemsInCart",
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofDays(30))
-                        .serializeValuesWith(RedisSerializationContext
-                                .SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(om.getTypeFactory()
-                                        .constructCollectionType(List.class, ItemInCart.class)))));
-    }
-
 }
